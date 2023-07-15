363bb0e6cc39d5884dc7d4fd3b851618914b2255
Nina <n1ska@outlook.com>
2023-07-15 20:29:44 +0300

    first commit

---
+++ src/main/java/ru/netotogy/unit/CashBackHacker.java
@@ -0,0 +1,10 @@
+package ru.netotogy.unit;
+
+public class CashBackHacker {
+
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        return boundary - amount % boundary;
+    }
+}
