5c2a0ba6575b31c5084fb1ebb6fd9042f1cd1451
Nina <n1ska@outlook.com>
2023-07-18 11:31:35 +0300

    task TestNG

---
+++ src/test/java/ru/netotogy/unit/CashBackHackerTest.java
@@ -0,0 +1,33 @@
+package ru.netotogy.unit;
+
+import static org.testng.Assert.assertEquals;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashBackHackerTest {
+
+    @org.testng.annotations.Test
+    void cash900 () {
+        CashBackHacker CashBackHacker = new CashBackHacker();
+        int amount = 900;
+
+        int actual = CashBackHacker.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void cash1000() {
+        CashBackHacker CashBackHacker = new CashBackHacker();
+        int amount = 1000;
+
+        int actual = CashBackHacker.remain(amount);
+        int expected = 0;
+
+       assertEquals(actual, expected);
+    }
+
+}
