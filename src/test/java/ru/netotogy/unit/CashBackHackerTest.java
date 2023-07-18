3027b66c67f1f291699caf2905d7721926feeeab
Nina <n1ska@outlook.com>
2023-07-18 19:54:11 +0300

    add tests

--- src/test/java/ru/netotogy/unit/CashBackHackerTest.java
+++ src/test/java/ru/netotogy/unit/CashBackHackerTest.java
@@ -1,4 +1,27 @@
+package ru.netotogy.unit;
+
 import junit.framework.TestCase;
-public class CashBackHackerTest extends TestCase {
-  
+
+public class CashBackHackerTest {
+    @org.junit.Test
+    public void cash900 () {
+        CashBackHacker CashBackHacker = new CashBackHacker();
+        int amount = 900;
+
+        int actual = CashBackHacker.remain(amount);
+        int expected = 100;
+
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+    @org.junit.Test
+    public void cash1000() {
+        CashBackHacker CashBackHacker = new CashBackHacker();
+        int amount = 1000;
+
+        int actual = CashBackHacker.remain(amount);
+        int expected = 0;
+
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
 }
